f99de8b2d9b62d96564b17ae3febddf38e31b456
Thiago Boneth <thiiboneth@gmail.com>
2021-02-08 21:24:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const xtudo = document.querySelector('.xtudo');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    xtudo.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+        xtudo.classList.toggle('toggle');
+    })
+
+}
+
+navSlide();
